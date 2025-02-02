c43543586309e8d7d72d0ca049219e2ec56f9341
Anuj Vijjan <anujvijjan10@gmail.com>
2025-02-02 19:56:39 +0530

    feat: Add github code review reminder cron logic

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,7 +1,13 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from .slack_bot import send_daily_summary
+from .slack_bot import send_daily_summary, send_code_review_reminder
 
 def start_scheduler():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(send_daily_summary, "cron", hour=9, minute=0)
+    
+    # Send daily task summary at 9 AM
+    scheduler.add_job(send_daily_summary, "cron", hour=10, minute=0)
+
+    # Send PR review reminders at 10 AM
+    scheduler.add_job(send_code_review_reminder, "cron", hour=10, minute=0)
+
     scheduler.start()
